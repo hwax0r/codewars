77921b3cb7134e6dbab2d25e2a5fe8a5a454345e
hwax0r <Sergeevdavid38@gmail.com>
2020-01-03 23:50:52 +0300

    my codewars repository initialization

---
+++ persistent_bugger.py
@@ -0,0 +1,42 @@
+'''
+Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence,
+which is the number of times you must multiply the digits in num until you reach a single digit.
+
+For example:
+
+ persistence(39) => 3  # Because 3*9 = 27, 2*7 = 14, 1*4=4
+                       # and 4 has only one digit.
+
+ persistence(999) => 4 # Because 9*9*9 = 729, 7*2*9 = 126,
+                       # 1*2*6 = 12, and finally 1*2 = 2.
+
+ persistence(4) => 0   # Because 4 is already a one-digit number.
+
+ persistence(39) # returns 3, because 3*9=27, 2*7=14, 1*4=4
+                 # and 4 has only one digit
+
+ persistence(999) # returns 4, because 9*9*9=729, 7*2*9=126,
+                  # 1*2*6=12, and finally 1*2=2
+
+ persistence(4) # returns 0, because 4 is already a one-digit number.
+'''
+
+def persistence(n):
+    if n < 10:
+        return 0
+    else:
+        counter = 0
+        res = 1
+        while n > 9:
+            digits = list(str(n))
+            print(digits)
+            for every in digits:
+                res*= int(every)
+            print(res)
+            n = res
+            res = 1
+            counter +=1
+        return counter
+
+print(persistence(431278)) # -> 4 (four is the number of times you must multiply the digits in num until you reach a single digit)
+# made two or three attempts before success
